1b74d8c9dcd711727db1c8f642788f0926a9052e
Raj Amal <raj.amalw@gmail.com>
2017-03-07 23:11:37 +0530

    Update to version 3.2.0

--- src/main/java/org/opencv/core/Core.java
+++ src/main/java/org/opencv/core/Core.java
@@ -17,10 +17,10 @@ import org.opencv.utils.Converters;
 public class Core {
 
     // these constants are wrapped inside functions to prevent inlining
-    private static String getVersion() { return "3.1.0"; }
-    private static String getNativeLibraryName() { return "opencv_java310"; }
+    private static String getVersion() { return "3.2.0"; }
+    private static String getNativeLibraryName() { return "opencv_java320"; }
     private static int getVersionMajor() { return 3; }
-    private static int getVersionMinor() { return 1; }
+    private static int getVersionMinor() { return 2; }
     private static int getVersionRevision() { return 0; }
     private static String getVersionStatus() { return ""; }
 
@@ -170,7 +170,20 @@ public class Core {
             FONT_HERSHEY_COMPLEX_SMALL = 5,
             FONT_HERSHEY_SCRIPT_SIMPLEX = 6,
             FONT_HERSHEY_SCRIPT_COMPLEX = 7,
-            FONT_ITALIC = 16;
+            FONT_ITALIC = 16,
+            ROTATE_90_CLOCKWISE = 0,
+            ROTATE_180 = 1,
+            ROTATE_90_COUNTERCLOCKWISE = 2,
+            TYPE_GENERAL = 0,
+            TYPE_MARKER = 0+1,
+            TYPE_WRAPPER = 0+2,
+            TYPE_FUN = 0+3,
+            IMPL_PLAIN = 0,
+            IMPL_IPP = 0+1,
+            IMPL_OPENCL = 0+2,
+            FLAGS_NONE = 0,
+            FLAGS_MAPPING = 0x01,
+            FLAGS_EXPAND_SAME_NAMES = 0x02;
 
 
     //
@@ -1103,6 +1116,20 @@ public class Core {
 
 
     //
+    // C++:  void convertFp16(Mat src, Mat& dst)
+    //
+
+    //javadoc: convertFp16(src, dst)
+    public static void convertFp16(Mat src, Mat dst)
+    {
+        
+        convertFp16_0(src.nativeObj, dst.nativeObj);
+        
+        return;
+    }
+
+
+    //
     // C++:  void convertScaleAbs(Mat src, Mat& dst, double alpha = 1, double beta = 0)
     //
 
@@ -1933,6 +1960,20 @@ public class Core {
 
 
     //
+    // C++:  void rotate(Mat src, Mat& dst, int rotateCode)
+    //
+
+    //javadoc: rotate(src, dst, rotateCode)
+    public static void rotate(Mat src, Mat dst, int rotateCode)
+    {
+        
+        rotate_0(src.nativeObj, dst.nativeObj, rotateCode);
+        
+        return;
+    }
+
+
+    //
     // C++:  void scaleAdd(Mat src1, double alpha, Mat src2, Mat& dst)
     //
 
@@ -1992,6 +2033,20 @@ public class Core {
     {
         
         setNumThreads_0(nthreads);
+        
+        return;
+    }
+
+
+    //
+    // C++:  void setRNGSeed(int seed)
+    //
+
+    //javadoc: setRNGSeed(seed)
+    public static void setRNGSeed(int seed)
+    {
+        
+        setRNGSeed_0(seed);
         
         return;
     }
@@ -2375,6 +2430,9 @@ public class Core {
     private static native void completeSymm_0(long mtx_nativeObj, boolean lowerToUpper);
     private static native void completeSymm_1(long mtx_nativeObj);
 
+    // C++:  void convertFp16(Mat src, Mat& dst)
+    private static native void convertFp16_0(long src_nativeObj, long dst_nativeObj);
+
     // C++:  void convertScaleAbs(Mat src, Mat& dst, double alpha = 1, double beta = 0)
     private static native void convertScaleAbs_0(long src_nativeObj, long dst_nativeObj, double alpha, double beta);
     private static native void convertScaleAbs_1(long src_nativeObj, long dst_nativeObj);
@@ -2526,6 +2584,9 @@ public class Core {
     // C++:  void repeat(Mat src, int ny, int nx, Mat& dst)
     private static native void repeat_0(long src_nativeObj, int ny, int nx, long dst_nativeObj);
 
+    // C++:  void rotate(Mat src, Mat& dst, int rotateCode)
+    private static native void rotate_0(long src_nativeObj, long dst_nativeObj, int rotateCode);
+
     // C++:  void scaleAdd(Mat src1, double alpha, Mat src2, Mat& dst)
     private static native void scaleAdd_0(long src1_nativeObj, double alpha, long src2_nativeObj, long dst_nativeObj);
 
@@ -2538,6 +2599,9 @@ public class Core {
 
     // C++:  void setNumThreads(int nthreads)
     private static native void setNumThreads_0(int nthreads);
+
+    // C++:  void setRNGSeed(int seed)
+    private static native void setRNGSeed_0(int seed);
 
     // C++:  void sort(Mat src, Mat& dst, int flags)
     private static native void sort_0(long src_nativeObj, long dst_nativeObj, int flags);
